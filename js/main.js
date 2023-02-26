f2e5dd0c9168c2cec0ba7f409c35724a382560da
MaxGaynov <makcs17@mail.ru>
2023-02-27 01:58:25 +0300

    4 задание готово

--- js/main.js
+++ js/main.js
@@ -16,12 +16,11 @@ const NAMES = [
 ];
 
 const MESSAGE =
-`Всё отлично! В целом всё неплохо. Но не всё.
-Когда вы делаете фотографию, хорошо бы убирать палец из кадра.
-В конце концов это просто непрофессионально.
-Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.
-Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.
-Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`;
+`Всё отлично! В целом всё неплохо. Но не всё. Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально. Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше. Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше. Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`;
+
+const arrMessage = MESSAGE.split('! ', 1).join();
+const finalArrMessage = MESSAGE.slice(13).split('. ');
+finalArrMessage.unshift(arrMessage);
 
 const generateId = createRandomUniqId (1, 25);
 const generateUrl = createRandomUniqId (1, 25);
@@ -35,25 +34,38 @@ const avatarIndex = () => {
   return `img/avatar-${getRandomInteger(1, 6)}.svg`;
 };
 
-const createMessage = (allComment) => {
-  const ARRMESSAGE = string.split('. ');
-  const newMessage = ARRMESSAGE[getRandomInteger(0, 8)] + '.' +  ARRMESSAGE[getRandomInteger(0, 8)] + '.';
+const createMessage = (array) => {
+  let newMessage = getRandomInteger(0, array.length - 1);
+  if (newMessage === array.length - 1) {
+    newMessage = array[array.length - 1];
+  }
+  else if (newMessage === 0) {
+    newMessage = array[0] + '!';
+  }
+  else {
+    newMessage = array[getRandomInteger(0, array.length - 2)] + '.';
+  }
   return newMessage;
 };
 
+const similarObjectsCount = 25;
+
 const createObject = () => {
-const profileFoto = {
-  id: generateId(),
-  url: urlIndex(),
-  description: 'оцените фотку!)',
-  likes: getRandomInteger(15, 200),
-  comments: {
+  const profileFoto = {
+    id: generateId(),
+    url: urlIndex(),
+    description: 'оцените фотку!)',
+    likes: getRandomInteger(15, 200),
+    comments: {
     id: generateCommentsId(),
     avatar: avatarIndex(),
-    message: createMessage(MESSAGE),
+    message: createMessage(finalArrMessage),
     name: showArrayRand(NAMES),
-  },
-}
+    },
+  };
+  return profileFoto;
 };
 
+const createSimilarObjects = () => Array.from({length: similarObjectsCount}, createObject);
 
+createSimilarObjects();
