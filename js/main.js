112469f70ff13cd7b188a069a65e4854d0dd6ded
MaxGaynov <makcs17@mail.ru>
2023-03-30 21:37:15 +0300

    11 задание правки автоматической проверки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import {similarObjectsCount} from './data.js';
+/* import {similarObjectsCount} from './data.js';
 import {createSimilarObjects} from './functions.js';
-import {createObject} from './generate-objects.js';
+import {createObject} from './generate-objects.js'; */
 import {generateNewMiniatures} from './generate-miniatures.js';
 import {openUserPicture} from './open-big-picture.js';
 import {closeUserModal} from './open-user-form.js';
@@ -10,13 +10,13 @@ import './slider-effects.js';
 
 //const descriptionObjects = createSimilarObjects(similarObjectsCount, createObject);
 //generateNewMiniatures(descriptionObjects);
-
 fetch('https://28.javascript.pages.academy/kekstagram/data')
-.then((response) => response.json())
-.then((miniatures) => {
-  console.log(miniatures)
-  generateNewMiniatures(miniatures)
-  openUserPicture(miniatures);
-});
+  .then((response) => response.json())
+  .then((miniatures) => {
+    console.log(miniatures);
+    generateNewMiniatures(miniatures);
+    openUserPicture(miniatures);
+  }
+);
 
 setUserFormSubmit(closeUserModal);
